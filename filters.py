8c0fc4db485db546b6334db03589e04da63932a2
Armen <harmen24@mail.ru>
2023-03-17 22:21:12 +0400

    Update filters.py

--- filters.py
+++ filters.py
@@ -15,6 +15,15 @@ def contrast(img: np.ndarray, value: int = 30):
 def laplacian(img: np.ndarray, kernel: np.ndarray = np.array([[0, 1, 0],
                                                               [1, -4, 1],
                                                               [0, 1, 0]]), scale: int = 1):
+    # """Laplacian edge detection"""
+    # cols = (img.shape[0] - kernel.shape[0]) // scale + 1
+    # rows = (img.shape[1] - kernel.shape[0]) // scale + 1
+    #
+    # lst = np.empty((cols, rows), dtype="int32")
+    #
+    # for i in range(0, cols * scale, scale):
+    #     for j in range(0, rows * scale, scale):
+    #         lst[i // scale, j // scale] = (np.sum(img[i: i + kernel.shape[0], j: j + kernel.shape[0]] * kernel))
     img_filter = gray_imp(img)
     return signal.convolve2d(img_filter, kernel)
 
@@ -22,6 +31,18 @@ def laplacian(img: np.ndarray, kernel: np.ndarray = np.array([[0, 1, 0],
 def gaussian_blur(img0: np.ndarray, kernel: np.ndarray = np.array([[0.0625, 0.125, 0.0625],
                                                                    [0.125, 0.25, 0.125],
                                                                    [0.0625, 0.125, 0.0625]])):
+    # s = 1
+    # cols = (img.shape[0] - kernel.shape[0]) // s + 1
+    # rows = (img.shape[1] - kernel.shape[0]) // s + 1
+    #
+    # lst = np.empty((cols, rows), dtype="int32")
+
+    # convolution implement but it slower. Now I use
+    # for i in range(0, cols * s, s):
+    #     for j in range(0, rows * s, s):
+    #         lst[i // s, j // s] = (np.sum(img[i: i + kernel.shape[0], j: j + kernel.shape[0]] * kernel))
+    
+    
     """ Remove noise """
     r, g, b = cv2.split(img0)
     r = signal.convolve2d(r, kernel)
@@ -31,6 +52,8 @@ def gaussian_blur(img0: np.ndarray, kernel: np.ndarray = np.array([[0.0625, 0.12
     b = signal.convolve2d(b, kernel)
     b = np.pad(b, 2)
     return cv2.merge([r, g, b])
+
+    # return cv2.GaussianBlur(img0, (3, 3), 0)
 
 
 def gray_imp(img0):
